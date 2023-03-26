eb497e817842d64de49a540c3ab0b0e001297949
Minsu Choi <cms102510@naver.com>
2023-03-26 15:00:49 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,19 @@
+mylist = [3,5,4,9,1,8,2,1]
+newlist =[i for i in mylist if (i%2)==0]
+print(newlist)
+
+
+student = { }
+student['지훈'] = 1234
+print(student)
+
+student['수민'] = 2345
+print(student)
+
+
+fruits = {'사과', '오렌지', '포도'}
+fruits.add('키위')
+print(fruits)
+
+
+
